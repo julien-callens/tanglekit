b36b9ed4f0ddad709b7f2cb92cebada71dc6b6be
SSSPPPOTTT <julien2000.callens@gmail.com>
2024-12-06 20:48:04 +0100

    updating variable ctx

--- jsb-compiler/output.js
+++ jsb-compiler/output.js
@@ -1,15 +1,15 @@
 function createFragment(ctx) {
-    const div = document.createElement('div')
-    div.setAttribute('class', '$.attributes[attr].content.value}')
-    const SQXvAKlT = document.createElement('p')
-    SQXvAKlT.innerHTML += test
-    div.appendChild(SQXvAKlT)
-    const aIngHxcu = document.createElement('p')
-    aIngHxcu.innerHTML += testing
-    div.appendChild(aIngHxcu)
-    div.innerHTML += 'test'
+const div = document.createElement('div');
+div.setAttribute('class', '$.attributes[attr].content.value}');
+const siFmEfPT = document.createElement('p');
+siFmEfPT.innerHTML += test;
+div.appendChild(siFmEfPT);
+const oANyDTNP = document.createElement('p');
+oANyDTNP.innerHTML += testing;
+div.appendChild(oANyDTNP);
+div.innerHTML += 'test';
 
-    return div
+return div;
 }
-const ctx = { variable, function, imports }
-document.body.appendChild(createFragment(ctx))
+const ctx = { variable, function, imports };
+document.body.appendChild(createFragment(ctx));
